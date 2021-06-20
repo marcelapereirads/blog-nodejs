f12d8751ccab5df90b59ad01af0e787ac60f53bf
Marcela <marcelapereirads@gmail.com>
2021-06-20 01:29:00 -0300

    CRUD Posts

--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,29 @@
 require('../models/Category');
+require('../models/Post');
 
 const express = require('express');
 const mongoose = require('mongoose');
 const Category = mongoose.model('categories');
+const Post = mongoose.model('posts');
 const router = express.Router();
 
 router.get('/', (req, resp) => resp.render('admin/index'));
 
-router.get('/posts', (req, resp) => resp.send('Posts'));
-
 router.get('/categories', (req, resp) => {
     Category.find()
         .then((categories) => resp.render('admin/categories', { categories: categories.map(cat => cat.toJSON()) }))
-        .catch((err) => {
-            console.log('Error listing category:', err);
-            addAlert(req, 'error', 'Error listing category');
-            redirectMain(resp);
+        .catch(() => {
+            addAlert(req, 'error', 'Error listing categories');
+            redirectMainCategory(resp);
         });
 });
 
-router.get('/categories/new', (req, resp) => resp.render('admin/form-category'));
+router.get('/categories/new', (req, resp) => resp.render('admin/category-form'));
 
 router.post('/categories/new', (req, resp) => {
+    const valid = validateCategory(req);
 
-    if (validateCategory(req)) {
+    if (valid) {
         const newCategory = {
             name: req.body.name,
             url: req.body.url
@@ -32,86 +32,195 @@ router.post('/categories/new', (req, resp) => {
         new Category(newCategory).save()
             .then(() => {
                 addAlert(req, 'success', 'Category created successfully');
-                redirectMain(resp);
+                redirectMainCategory(resp);
             })
-            .catch((err) => {
-                console.log('Error saving category:', err);
+            .catch(() => {
                 addAlert(req, 'error', 'Error saving category');
             });
-    } else {
-        redirectMain(resp);
     }
 });
 
 router.get('/categories/edit/:id', (req, resp) => {
     Category.findOne({ _id: req.params.id })
         .then((category) => {
-            resp.render('admin/form-category', { category: category.toJSON() });
+            resp.render('admin/category-form', { category: category.toJSON() });
         })
-        .catch((err) => {
-            console.log('Error querying category:', err);
+        .catch(() => {
             addAlert(req, 'error', 'Error querying category');
-            redirectMain(resp);
+            redirectMainCategory(resp);
         });
 });
 
 router.post('/categories/edit/:id', (req, resp) => {
-    Category.findOne({ _id: req.params.id })
-        .then((category) => {
-            category.name = req.body.name;
-            category.url = req.body.url;
+    const valid = validateCategory(req);
 
-            category.save()
-                .then(() => {
-                    addAlert(req, 'success', 'Category edited successfully');
-                    redirectMain(resp);
-                })
-                .catch((err) => {
-                    console.log('Error editing category:', err);
-                    addAlert(req, 'error', 'Error editing category');
-                    redirectMain(resp);
-                });
-        })
-        .catch((err) => {
-            console.log('Error editing category:', err);
-            addAlert(req, 'error', 'Error editing category');
-            redirectMain(resp);
-        })
+    if (valid) {
+        Category.findOne({ _id: req.params.id })
+            .then((category) => {
+                category.name = req.body.name;
+                category.url = req.body.url;
+
+                category.save()
+                    .then(() => {
+                        addAlert(req, 'success', 'Category edited successfully');
+                        redirectMainCategory(resp);
+                    })
+                    .catch(() => {
+                        addAlert(req, 'error', 'Error editing category');
+                        redirectMainCategory(resp);
+                    });
+            })
+            .catch(() => {
+                addAlert(req, 'error', 'Error editing category');
+                redirectMainCategory(resp);
+            })
+    }
+    
 });
 
 router.get('/categories/delete/:id', (req, resp) => {
     Category.remove({ _id: req.params.id })
         .then(() => {
             addAlert(req, 'success', 'Category deleted successfully');
-            redirectMain(resp);
+            redirectMainCategory(resp);
         })
-        .catch((err) => {
-            console.log('Error deleting category:', err);
+        .catch(() => {
             addAlert(req, 'error', 'Error deleting category');
-            redirectMain(resp);
+            redirectMainCategory(resp);
         });
 });
+
+router.get('/posts', (req, resp) => {
+    Post
+        .find()
+        .lean()
+        .populate('category')
+        .sort({ data: 'desc' })
+        .then(posts => resp.render('admin/posts', { posts
+         }))
+        .catch(() => {
+            addAlert(req, 'error', 'Error listing posts');
+            redirectMainPost(resp);
+        });
+});
+
+router.get('/posts/new', (req, resp) => {
+    Category
+        .find()
+        .then((categories) => resp.render('admin/post-form', { categories: categories.map(cat => cat.toJSON()) }))
+        .catch(() => {
+            addAlert(req, 'error', 'Error listing category');
+            redirectMainCategory(resp);
+        });
+});
+
+router.post('/posts/new', (req, resp) => {
+    if (validatePost(req)) {
+        const newPost = {
+            ...req.body
+        };
+
+        new Post(newPost).save()
+            .then(() => {
+                addAlert(req, 'success', 'Post created successfully');
+                redirectMainPost(resp);
+            })
+            .catch(() => {
+                addAlert(req, 'error', 'Error saving post');
+            });
+    }
+});
+
+router.get('/posts/edit/:id', (req, resp) => {
+    Post
+        .findOne({ _id: req.params.id })
+        .then(post => {
+            Category
+                .find()
+                .then(categories => {
+                    resp.render('admin/post-form', {
+                        categories: categories.map(categories => categories.toJSON()),
+                        post: post.toJSON()
+                    });
+                })
+                .catch(() => {
+                    addAlert(req, 'error', 'Error querying posts');
+                    redirectMainPost(resp);
+                })
+        })
+        .catch(() => {
+            addAlert(req, 'error', 'Error querying posts');
+            redirectMainPost(resp);
+        });
+});
+
+router.post('/posts/edit/:id', (req, resp) => {
+    if (validatePost(req)) {
+        Post
+            .findOne({ _id: req.params.id })
+            .then((post) => {
+                Object.entries(req.body).forEach(([key, value]) => post[key] = value);
+
+                post.save()
+                    .then(() => {
+                        addAlert(req, 'success', 'Post edited successfully');
+                        redirectMainPost(resp);
+                    })
+                    .catch(() => {
+                        addAlert(req, 'error', 'Error editing post');
+                        redirectMainPost(resp);
+                    });
+            })
+            .catch((err) => {
+                addAlert(req, 'error', 'Error editing post');
+                redirectMainPost(resp);
+            })
+    }
+});
+
+router.get('/post/delete/:id', (req, resp) => {
+    Post.remove({ _id: req.params.id })
+        .then(() => {
+            addAlert(req, 'success', 'Post deleted successfully');
+            redirectMainPost(resp);
+        })
+        .catch(() => {
+            addAlert(req, 'error', 'Error deleting post');
+            redirectMainPost(resp);
+        });
+});
+
 
 const addAlert = (req, type, message) => {
     req.flash(type === 'success' ? 'success_msg' : 'error_msg', message);
 }
 
-const redirectMain = (resp) => {
+const redirectMainCategory = (resp) => {
     resp.redirect('/admin/categories');
 }
 
-const validateCategory = (req) => {
-    if (!req.body.name) {
-        addAlert(req, 'error', 'Name is mandatory');
-        return false;
-    }
+const redirectMainPost = (resp) => {
+    resp.redirect('/admin/posts');
+}
 
-    if (!req.body.url) {
-        addAlert(req, 'error', 'URL is mandatory');
+const validateCategory = (req) => {
+    if (!req.body.name || !req.body.url) {
         return false;
     }
 
     return true;
+}
+
+const validatePost = (req) => {
+    let valid = true;
+    Object.entries(req.body)
+        .forEach(([key, value]) => {
+            if (!value) {
+                valid = false;
+            }
+        });
+
+    return valid;
 }
 
 module.exports = router;
